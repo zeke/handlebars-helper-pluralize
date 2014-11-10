c07619bcf4781cacfeddc87229423f96ac9c91c0
Zeke Sikelianos <zeke@sikelianos.com>
2014-11-10 15:52:07 -0800

    ugh loose typing

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var inflect = require("i")()
 
 module.exports = function(count, word, includeCount) {
   var result = Number(count) === 1 ? inflect.singularize(word) : inflect.pluralize(word)
-  if (includeCount) result = String(count) + " " + result
+
+  if (includeCount === true || includeCount === "true") {
+    result = String(count) + " " + result
+  }
+
   return result
 }
